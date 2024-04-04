7a78eefc2cd262f96f5fdf356aeda63c6cbcd9e0
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-04-04 12:10:24 +0500

    add project angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  styles: `
+		select.form-select {
+			margin: 0.5rem 0.5rem 0 0;
+			width: auto;
+		}
+	`,
+  
 })
 export class AppComponent {
   title = 'my-app';
+  displayMonths = 2;
+	navigation = 'select';
+	showWeekNumbers = false;
+	outsideDays = 'visible';
 }
